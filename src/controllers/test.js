aae69f58dacb29fbdea111364cce26b815895f42
lwyj123 <443474713@qq.com>
2018-07-05 19:53:20 +0800

    feat: init using lw-cli

---
+++ src/controllers/test.js
@@ -0,0 +1,105 @@
+
+// const path = require('path')
+const BaseController = require('./prototype/BaseController')
+const ArticleModal = require('../models/article')
+const pagination = require('../helpers/page')
+
+class Test extends BaseController {
+  async GetArticleList (ctx) {
+    const query = ctx.query
+    const searchQuery = {}
+    if (false) {
+      searchQuery.$and = []
+    }
+    if (false) {
+      searchQuery.$or = []
+    }
+    const list = await ArticleModal
+      .find(searchQuery)
+      .sort({ updated_at: -1 })
+
+    const data = pagination.getCurrentPageDataWithPagination(
+      list.map(item => item.toObject()),
+      query.offset + 1,
+      query.limit
+    )
+    ctx.body = {
+      ...data
+    }
+  }
+
+  async test (ctx) {
+    ctx.body = {
+      'test': 'wori'
+    }
+  }
+}
+
+// 支持筛选、分页
+const GetArticleList = async (ctx) => {
+  const query = ctx.query
+  const searchQuery = {}
+  if (false) {
+    searchQuery.$and = []
+  }
+  if (false) {
+    searchQuery.$or = []
+  }
+  const list = await ArticleModal
+    .find(searchQuery)
+    .sort({ updated_at: -1 })
+
+  const data = pagination.getCurrentPageDataWithPagination(
+    list.map(item => item.toObject()),
+    query.offset + 1,
+    query.limit
+  )
+  ctx.body = {
+    ...data
+  }
+}
+
+const GetArticleById = async (ctx) => {
+  const articleId = ctx.params.id
+  console.log(articleId)
+
+  const articleDoc = await ArticleModal
+    .findByIdAndUpdate(articleId, { $inc: { read_count: 1 } })
+
+  ctx.body = {
+    ...articleDoc.toObject()
+  }
+}
+
+const PostArticle = async (ctx) => {
+  let data = ctx.request.body
+
+  if (!data.title || !data.content) {
+    ctx.body = {
+      err: 'require necessary filed'
+    }
+    ctx.response.status = 422
+    return
+  }
+  var articleModal = new ArticleModal({
+    title: data.title,
+    content: data.content
+  })
+  await articleModal.save(function (err, articleDoc) {
+    if (err) {
+      // throw new Error(err.toString())
+      ctx.body = {
+        err: err.errmsg
+      }
+      ctx.response.status = 422
+    } else {
+      ctx.body = {
+        ...articleDoc.toObject()
+      }
+    }
+  }).catch((err) => {
+    console.log(err.errmsg)
+  })
+}
+
+module.exports = new Test()
